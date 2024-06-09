6e543663a56f0656a9d022178689504e1fcb9544
Nick <kkkygsos@naver.com>
2024-06-09 18:40:21 +0900

    240609
    240609

---
+++ 3. input_output_functions_and_operators/3-1. input_two_integers.c
@@ -0,0 +1,13 @@
+// 두 정수를 입력받아 출력하는 프로그램
+
+#include <stdio.h>
+void main()
+{
+    int a, b;
+
+    printf("input(integer)==>");
+    scanf("%d", &a);
+    printf("input(integer)=>");
+    scanf("%d", &b);
+    printf("\na= %d, b= %d", a, b);
+}
